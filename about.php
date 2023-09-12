7c92bb5dcd938f12c83351ee61e7b3068482f5f1
Joe-metry <joemetry150@gmail.com>
2023-09-12 00:42:48 -0700

    About PHP

---
+++ about.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+        <title>About Us</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="About.css">
+
+        <link rel ="icon" href="img/about_50px.png" type="image/x-icon">
+
+    </head>
+
+    <body>
+        <section>
+            <div class = "image">
+                
+            </div>
+
+            <div class = "content">
+                
+            <h2>About Us</h2><br><br>
+
+            <span></span>
+
+                <p><b> WEB DESIGN BY : TEAM DESK DEMON
+
+                <br><br>DESIGN BY : DESHAN, FRANKELLY, JOEMETRY && AYANSWAG.
+            
+                <br><br> GROUP MEMBERS :<br><br>
+
+                Ayan, Nicholas – AA2116<br><br>
+
+                Frankelly, Ozonwu – AA2102<br><br>
+                
+                Ubi, Joseph – AA1689<br><br>
+
+                </p>
+
+            </div>
+        </section>
+        
+    </body>
+</html>
