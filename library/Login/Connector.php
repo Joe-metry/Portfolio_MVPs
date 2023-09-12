082e265f81a78226ea105b4d89d436d5ad898453
Joe-metry <joemetry150@gmail.com>
2023-09-12 05:46:43 -0700

    Login connector engine

---
+++ library/Login/Connector.php
@@ -0,0 +1,20 @@
+<?php
+
+$sname= "localhost";
+
+$unmae= "root";
+
+$password = "";
+
+$db_name = "library";
+
+$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+
+if (!$conn) 
+{
+	echo "Connection failed!";
+}
+else
+{
+    echo "Connection Established";
+}
